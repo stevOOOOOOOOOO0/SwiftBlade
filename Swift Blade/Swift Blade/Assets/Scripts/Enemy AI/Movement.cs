cbd37ea876fc60d295387a6638236c45fd210202
Stevo <stevOOOOOOOOOO0@yahoo.com>
2019-01-16 11:28:04 -0700

    Enemy AI rotation finished

--- Swift Blade/Swift Blade/Assets/Scripts/Enemy AI/Movement.cs
+++ Swift Blade/Swift Blade/Assets/Scripts/Enemy AI/Movement.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 	
-	public Vector3 _newPosition;
+	public Transform _newPosition;
 	private Vector3 _speed;
-	public float acceleration = 5;
-	public float _maxSpeed = 200;
-	private Rigidbody _ship;
+	public float _acceleration = 5;
+	public float _drag = 5;
 	public float SmoothSpeed = 5;
-	private rotation _desiredRotation;
+	public float _maxSpeed = 40;
+	private Rigidbody _ship;
+	private Quaternion _desiredRotation;
 
 	// Use this for initialization
 	void Start () {	
@@ -21,17 +22,16 @@ public class Movement : MonoBehaviour {
 	void Update () {
 
 		//rotation is handled here
-
-		_desiredRotation = Quaternion.LookRotation(_newPosition - transform.position);
-		transform.rotation = Quaternion.Slerp(transfrom.rotation, _desiredRotation, SmoothSpeed * Time.deltatime);
+		_desiredRotation = Quaternion.LookRotation(_newPosition.position - transform.position);
+		transform.rotation = Quaternion.Slerp(transform.rotation, _desiredRotation, SmoothSpeed * Time.deltaTime);
 
 		//position is handled here
-
-		_speed += transform.forward * acceleration;
+		if (transform.rotation == _desiredRotation)
+			_speed += transform.forward * _acceleration;
+		else if (_speed != Vector3.zero)
+			_speed -= transform.forward * -_drag;
 		if (_speed.magnitude > _maxSpeed)
-		{
-			_speed += _speed.normalized * _maxSpeed
-		}
+			_speed = _speed.normalized * (_maxSpeed - 1);
 		_ship.velocity = _speed;
 	}
 }
