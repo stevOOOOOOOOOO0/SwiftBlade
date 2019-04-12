9cc67ef70205953765acf9578748cd1ddac37ef1
stevOOOOOOOOOO0 <stevOOOOOOOOOO0@yahoo.com>
2019-04-12 02:14:18 -0600

    Update Movement.cs

--- Swift Blade/Swift Blade/Assets/Scripts/Enemy AI/Movement.cs
+++ Swift Blade/Swift Blade/Assets/Scripts/Enemy AI/Movement.cs
@@ -21,18 +21,17 @@ public class Movement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+			//Moves the Enemy Ship towards the set location
 		_ship.MovePosition(Vector3.Lerp(transform.position, _newPosition, .1f * Time.deltaTime));
-
+			//Rotates the ship to face the next waypoint
 		_ship.MoveRotation(Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_newPosition - transform.position), .1f));
-
+			//counts the timer up
 		_timer += 1 * Time.deltaTime;
-
+			//once the timer has hit 5 it jumps into this loop to set a new waypoint and reset the timer
 		if (_timer >= 5)
 		{
 			_newPosition.Set(Random.Range(-RangeOfSpawn, RangeOfSpawn), Random.Range(-RangeOfSpawn, RangeOfSpawn), Random.Range(-RangeOfSpawn, RangeOfSpawn));
 			_timer = 0;
 		}
-		//print("Distance: " + Vector3.Distance(transform.position, _newPosition));
 	}
 }
