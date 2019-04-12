21e584a6b2c2fe235c456114c092e02b11862aea
Laptop <StevOOOOOOOOO0@YAHOO.com>
2019-04-12 01:58:20 -0600

    edited notes

--- Swift Blade/Swift Blade/Assets/Scripts/Camera/CameraFollow.cs
+++ Swift Blade/Swift Blade/Assets/Scripts/Camera/CameraFollow.cs
@@ -25,23 +25,34 @@ public class CameraFollow : MonoBehaviour
 
 		if (!_freeRotate)
 		{
+			//set the desired position to the offset distance behind the players current position
 			Vector3 desiredPosition = Target.position - (Target.forward * Offset - Target.up * 5);
+			//move the camera to a position between the current position and the desired position based on the smoothspeed
 			transform.position = Vector3.Lerp(transform.position, desiredPosition, SmoothSpeed);
-			
+
+			//set the desiredRotation to the rotation of the Player
 			Quaternion desiredRotation = Target.rotation;
+			//change the Quaternion(Looking direction) to the desired rotation
 			transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, SmoothSpeed);
 		}
 
 		else
 		{
+			//get the rotation around player in the Y axis according to mouse movement
 			transform.RotateAround(Target.position, transform.right, Input.GetAxis("Mouse Y") * CameraRotateSpeed * Time.deltaTime);
+			//get the rotation around player in the X axis according to mouse movement
 			transform.RotateAround(Target.position, transform.up, Input.GetAxis("Mouse X") * CameraRotateSpeed * Time.deltaTime);
-			//transform.LookAt(Target, transform.up);
-			_desiredRotation = Quaternion.LookRotation(Target.position - transform.position);
-			transform.rotation = Quaternion.Slerp(transform.rotation, _desiredRotation, SmoothSpeed * Time.deltaTime);
 
+			//get the desired quaternion(looking direction) of the camera
+			_desiredRotation = Quaternion.LookRotation(Target.position - transform.position);
+			//set the rotation of the camera quaternion to the desired rotation softly
+			transform.rotation = Quaternion.Slerp(transform.rotation, _desiredRotation, SmoothSpeed * Time.deltaTime);
+			
+			//find the actual movement of the camera
 			_movement = Target.position - _oldPosition;
+			//change _oldPosition to the new Position of the camera
 			_oldPosition = Target.position;
+			//intiate the movement of the camera
 			transform.position += _movement;
 		}
 	}
