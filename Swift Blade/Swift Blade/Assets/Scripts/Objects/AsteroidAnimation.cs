930e1636786d567c69926ed11ecbf950b967e878
Stephen <stevOOOOOOOOOO0@YAHOO.COM>
2019-04-23 01:21:57 -0600

    finished the animation controller
    requirements for the assignment 4 animation

---
+++ Swift Blade/Swift Blade/Assets/Scripts/Objects/AsteroidAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidAnimation : MonoBehaviour {
+
+	public Animator anim;
+	public float increase = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.speed = 0.5f + increase;
+		increase += Time.deltaTime * (.1f/2);
+	}
+}
