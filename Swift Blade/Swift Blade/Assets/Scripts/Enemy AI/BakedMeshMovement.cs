e065efe500fa8db2076ba97d1a1b82c32a62f224
Stevo <stevOOOOOOOOOO0@yahoo.com>
2019-04-24 12:01:14 -0600

    added more assets and worked on enemy AI

---
+++ Swift Blade/Swift Blade/Assets/Scripts/Enemy AI/BakedMeshMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BakedMeshMovement : MonoBehaviour {
+
+	public Transform Target;
+	public float Range;
+	private bool tracking = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (tracking)
+		{
+			transform.position.Set(transform.position.x, Target.position.y, transform.position.z);
+			print("double yes");
+			return;
+		}
+		
+		else if (Vector3.Distance(Target.position, transform.position) < Range)
+		{
+			transform.position.Set(0, Target.position.y, 0);
+			tracking = true;
+			print("got em");
+		}
+	}
+}
---
+++ Swift Blade/Swift Blade/Assets/Scripts/Enemy AI/NavMeshMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Services;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshMovement : MonoBehaviour
+{
+
+	public NavMeshAgent Nav;
+	public Transform Target;
+	public float Range;
+	private bool tracking = false;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		Nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (tracking)
+		{
+			Nav.destination = Target.position;
+			return;
+		}
+		
+		if (Vector3.Distance(Target.position, transform.position) < Range)
+		{
+			Nav.destination = Target.position;
+			tracking = true;
+		}
+	}
+}
