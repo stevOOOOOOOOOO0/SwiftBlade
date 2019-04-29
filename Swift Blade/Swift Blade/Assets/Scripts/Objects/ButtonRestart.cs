a06429f82c5190be8bf8ac683b3a5839c251b85f
Stevo <stevOOOOOOOOOO0@yahoo.com>
2019-04-29 11:07:14 -0600

    fixed lighting

---
+++ Swift Blade/Swift Blade/Assets/Scripts/Objects/ButtonRestart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ButtonRestart : MonoBehaviour
+{
+
+    public int LoadScene;
+    
+    public void Restart()
+    {
+        //Application.LoadLevel(Application.loadedLevel);
+        SceneManager.LoadScene(LoadScene);
+    }
+    
+}
